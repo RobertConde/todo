dac21de1e3a1ae58c695a59d7cdba1d86920e24c
Robert Conde <rconde2003@gmail.com>
2024-11-09 22:40:28 -0500

    Favicon + web app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,14 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href="/favicon.png" />
+
+	<!-- Apple Web App -->
+	<link rel="apple-touch-icon" href="/favicon.png" />
+	<meta name="apple-mobile-web-app-title" content="Todo" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+</svelte:head>
+
 {@render children()}
